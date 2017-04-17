c3b1dd10c3a5e3e05a7c8038ada9e391b3644483
Dalyoung <Dalyoung@users.noreply.github.com>
2017-04-17 09:51:18 +0900

    Update Solution.java

--- Codility/src/lesson06/Triangle/Solution.java
+++ Codility/src/lesson06/Triangle/Solution.java
@@ -14,20 +14,13 @@ public class Solution {
 	
 	public int solution(int[] A) {
         // write your code in Java SE 8
-		if(A.length < 3){
-			return 0;
-		}
 		Arrays.sort(A);
 //		System.out.println(Arrays.toString(A));
 		for(int i = 0; i < A.length - 2; i++){
-			if( ((long)(A[i] + A[i+1]) > A[i+2])
-					&& ((long)(A[i+1] + A[i+2]) > A[i])
-					&& ((long)(A[i+2] + A[i]) > A[i+1])
-					){
+			if((long)A[i] + (long)A[i+1] > (long)A[i+2]){
 				return 1;
 			}
 		}
-		return 0;
     }
 	
 }
