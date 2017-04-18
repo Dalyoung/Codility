0816ac9c9db99533a708a6cdd9f7d21820385ca9
Dalyoung <Dalyoung@users.noreply.github.com>
2017-04-19 08:15:11 +0900

    Create Solution.java

---
+++ Codility/src/lesson7/StoneWall/Solution.java
@@ -0,0 +1,36 @@
+package lesson7.StoneWall;
+
+import java.util.Stack;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Solution s = new Solution();
+		System.out.println(s.solution(new int[]{8, 8, 5, 7, 9, 8, 7, 4, 8}));
+	}
+	public int solution(int[] H) {
+        // write your code in Java SE 8
+		int ret = 0;
+		Stack<Integer> s = new Stack<>();
+		for(int i = 0; i < H.length; i++){
+			if(s.isEmpty()){
+				s.push(H[i]);
+			}else{
+				while(!s.isEmpty()){
+					if(s.peek() <= H[i]){
+						break;
+					}else if(s.peek() > H[i]){
+						s.pop();
+						ret++;
+					}
+				}
+				if(s.isEmpty() || s.peek() != H[i]){
+					s.push(H[i]);
+				}
+			}
+		}
+//		System.out.println(Arrays.toString(s.toArray()));
+		return ret + s.size();
+    }
+	
+}
