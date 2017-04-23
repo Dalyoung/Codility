7e9264cbe00e5fb7176bd70191ae0f16bb3d0d1d
Dalyoung <Dalyoung@users.noreply.github.com>
2017-04-24 08:12:13 +0900

    Update Solution.java

--- Codility/src/lesson09/MaxDoubleSliceSum/Solution.java
+++ Codility/src/lesson09/MaxDoubleSliceSum/Solution.java
@@ -1,32 +1,32 @@
 package lesson09.MaxDoubleSliceSum;
 
+import java.util.Arrays;
 
 public class Solution {
 
 	public static void main(String[] args) {
 		Solution s = new Solution();
-		System.out.println(s.solution(30));
-		System.out.println(s.solution(100000000));
+		System.out.println(s.solution(new int[]{3, 2, 6, -1, 4, 5, -1, 2}));
+		System.out.println(s.solution(new int[]{6, 1, 5, 6, 4, 2, 9, 4})); // 26
 	}
-	public int solution(int N) {
-        // write your code in Java SE 8
-		if(N == 1){
-			return 4;
+	public int solution(int[] A) {
+		int maxSum = 0;
+		int N = A.length;
+		int left[] = new int[N];
+		int right[] = new int[N];
+		int length = N - 2;
+		for(int i = 1; i < length; i++){
+			left[i] = Math.max(0, left[i-1] + A[i]);
+			right[N - i - 1] = Math.max(0, right[N - i] + A[N - i - 1]);
 		}
-		
-		int i = 1;
-		int temp;
-		int min = Integer.MAX_VALUE;
-		while(i * i <= N){
-			if(N % i == 0){
-				temp = N / i;
-				min = Math.min(min, 2 * (i + temp));
-				System.out.println(min);
-			}
-			i++;
+//		System.out.println(Arrays.toString(left));
+//		System.out.println(Arrays.toString(right));
+		maxSum = right[1];
+		for(int i = 0; i < length; i++){
+			maxSum = Math.max(maxSum, left[i] + right[i+2]);
 		}
+		return maxSum;
 		
-		return min;
     }
 	
 }
